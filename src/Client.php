cd1309eba22f62d2494aee7e393008a087e7cbc6
tanbin <1370808424@qq.com>
2018-03-08 11:24:46 +0800

    package

---
+++ src/Client.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by AK.
+ * Date: 2018/3/8
+ * Time: 10:24
+ */
+
+namespace Shiliangpie\TestPackage;
+
+
+class Client
+{
+    protected $num1;
+    protected $num2;
+
+    public function __construct(int $num1, int $num2)
+    {
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+    }
+
+    public function addTogether()
+    {
+        return $this->num1 + $this->num2;
+    }
+}
